55cd77f58d12548254ae07c7a1caef2cac1a70d3
Jan <Jan@Upper.fritz.box>
2021-02-18 16:51:48 +0100

    added move to moveboxes

--- Hexapawn/src/moveboxes/Move.java
+++ Hexapawn/src/moveboxes/Move.java
@@ -4,7 +4,7 @@ public class Move {
 	
 	/* A move is an action a Piece can perform in the game. 
 	 * 
-	 * Every Move has a colour assigned to it, so it can be identified.
+	 * Every Move has a color assigned to it, so it can be identified.
 	*
 	*/
 }
